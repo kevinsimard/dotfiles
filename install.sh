0d49c6260884d6075b45afe42636d3468d01a316
Kevin Simard <kev.simard@gmail.com>
2015-09-21 20:11:28 -0400

    Install 'junegunn/vim-plug' plugin

--- install.sh
+++ install.sh
@@ -9,9 +9,12 @@ ln -sf $(pwd)/vim/.vimrc ~/.vimrc
 ln -sf $(pwd)/vim/.vimrc.before ~/.vimrc.before
 ln -sf $(pwd)/vim/.vimrc.after ~/.vimrc.after
 
+# install 'junegunn/vim-plug' plugin
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # vim pluggins
 ln -s $(pwd)/vim/plugged ~/.vim/plugged
 
 # vim schemes
 ln -sf $(pwd)/vim/themes/predawn/colors/predawn.vim ~/.vim/colors/predawn.vim
-
