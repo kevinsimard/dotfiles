d9faa4eb4099bb83a9c556743cfb15e784b936e2
Kevin Simard <kev.simard@gmail.com>
2015-10-14 14:07:04 -0400

    Fix installation script

--- install.sh
+++ install.sh
@@ -1,33 +1,28 @@
 #!/usr/bin/env bash
 
-PWD=$(pwd)
-
 # vim directories
 mkdir -p ~/.vim/
 mkdir -p ~/.vim/colors/
 
 # vimrc files
-ln -sf $PWD/vim/.vimrc ~/.vimrc
-ln -sf $PWD/vim/.vimrc.before ~/.vimrc.before
-ln -sf $PWD/vim/.vimrc.after ~/.vimrc.after
+ln -sf $(pwd)/vim/.vimrc ~/.vimrc
+ln -sf $(pwd)/vim/.vimrc.before ~/.vimrc.before
+ln -sf $(pwd)/vim/.vimrc.after ~/.vimrc.after
 
 # vim pluggins
-ln -s $PWD/vim/plugged ~/.vim/plugged
+ln -s $(pwd)/vim/plugged ~/.vim/plugged
 
 # vim schemes
-ln -sf $PWD/vim/themes/predawn/colors/predawn.vim ~/.vim/colors/predawn.vim
+ln -sf $(pwd)/vim/themes/predawn/colors/predawn.vim ~/.vim/colors/predawn.vim
 
 # install 'junegunn/vim-plug' plugin
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # compile 'vimproc' native extension
-cd $PWD/vim/plugged/vimproc.vim/ && make ARCHS='i386 x86_64'
-
-# required for 'YouCompleteMe'
-brew install cmake
+cd ~/.vim/plugged/vimproc.vim/ && make ARCHS='i386 x86_64'
 
 # compile 'YouCompleteMe' binary
-cd $PWD/vim/plugged/YouCompleteMe/ && \
+cd ~/.vim/plugged/YouCompleteMe/ && \
     git submodule update --init --recursive && \
     ./install.py
