4b4d684e37c8612fc19d35d26b2dbe537d2bcd00
Kevin Simard <kev.simard@gmail.com>
2016-11-10 13:47:44 -0500

    Remove unused sublime files

--- install.sh
+++ install.sh
@@ -15,14 +15,13 @@ ln -s $(pwd)/vim/plugged ~/.vim/plugged
 # vim schemes
 ln -sf $(pwd)/vim/themes/predawn/colors/predawn.vim ~/.vim/colors/predawn.vim
 
-# install 'junegunn/vim-plug' plugin
+# install "junegunn/vim-plug" plugin
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-# compile 'vimproc' native extension
-cd ~/.vim/plugged/vimproc.vim/ && make ARCHS='i386 x86_64'
+# compile "vimproc" native extension
+cd ~/.vim/plugged/vimproc.vim/ && make ARCHS="i386 x86_64"
 
-# compile 'YouCompleteMe' binary
+# compile "YouCompleteMe" binary
 cd ~/.vim/plugged/YouCompleteMe/ && \
-    git submodule update --init --recursive && \
-    ./install.py
+    git submodule update --init --recursive && ./install.py
