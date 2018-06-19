03268c7ab617b0903d234ca3e615c11bf9e67161
Kevin Simard <kev.simard@gmail.com>
2018-06-19 13:33:21 -0400

    Update install.sh script commands

--- install.sh
+++ install.sh
@@ -1,22 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # directories
-mkdir -p ~/.vim/autoload/
 mkdir -p ~/.vim/colors/
+mkdir -p ~/.vim/autoload/
 
 # vimrc files
 ln -sf $(pwd)/vim/.vimrc ~/.vimrc
 ln -sf $(pwd)/vim/.vimrc.before ~/.vimrc.before
 ln -sf $(pwd)/vim/.vimrc.after ~/.vimrc.after
-ln -sf $(pwd)/vim/.vimrc ~/.ideavimrc
-
-# vim schemes
-ln -sf $(pwd)/vim/themes/predawn/colors/predawn.vim ~/.vim/colors/predawn.vim
 
 # vim plugins
 ln -sf $(pwd)/vim/plug.vim ~/.vim/autoload/plug.vim
 ln -sf $(pwd)/vim/plugged/ ~/.vim/plugged
-
-# editorconfig file
-ln -sf $(pwd)/.editorconfig ~/.editorconfig
-ln -sf $(pwd)/.editorconfig /Volumes/HD/.editorconfig
